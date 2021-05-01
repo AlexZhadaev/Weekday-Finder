21241c096462b750bbb7c1244d3a26f872296197
AlexZhadaev <pvt.vatnik@gmail.com>
2021-05-01 18:36:14 +0300

    Initial Commit

---
+++ Project practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project practice
+//
+//  Created by Жадаев Алексей on 01.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
